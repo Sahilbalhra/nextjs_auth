95a156baf4b8259dda791b7c8b929e1ee9cf7619
Sahilbalhra <sahilbalhra0@gmail.com>
2024-01-05 10:51:26 +0530

    register action added

---
+++ src/actions/register.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+
+import { RegisterSchema } from "@/schemas";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already exists !" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "Email sent!" };
+};
